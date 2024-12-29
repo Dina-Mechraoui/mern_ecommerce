65d428cafe410fce0fff68d831d997bee4953440
Dina <d.mechraoui8@gmail.com>
2024-12-29 05:52:07 +0100

    setting up server side

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,30 @@
-import express from 'express'
+import express from 'express';
 import connectDB from './config/db.js';
-import ProductRoutes from './routes/productRoutes.js'
+import ProductRoutes from './routes/productRoutes.js';
 import OrderRoutes from './routes/orderRoutes.js';
 import sessionMiddleware from './middleware/session.js';
-import CartRoutes from './routes/CartRoutes.js'
-import RegionRoutes from './routes/regionRoutes.js'
+import CartRoutes from './routes/CartRoutes.js';
+import RegionRoutes from './routes/regionRoutes.js';
 import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import AdminRoutes from './routes/AdminRoutes.js'
+import AdminRoutes from './routes/AdminRoutes.js';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+
 dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-connectDB()
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+app.use(helmet());
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
 
 const options = {
@@ -29,34 +34,36 @@ const options = {
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  },credentials: true,
+  },
+  credentials: true,
 };
 
 app.use(cors(options));
-app.use(sessionMiddleware)
+app.use(sessionMiddleware);
 
-app.get('/', (req, res) => {
-    if (!req.session.views) {
-        req.session.views = 1;
-    } else {
-        req.session.views++;
-    }
-    res.send(`You've visited this page ${req.session.views} times`);
-});
-app.get('/session', (req, res) => {
-    res.json(req.session);
-});
-
-app.use('/api/product',  ProductRoutes);
+app.use('/api/product', ProductRoutes);
 app.use('/api/cart', CartRoutes);
 app.use('/api/order', OrderRoutes);
-app.use('/api/region',  RegionRoutes);
-app.use('/api/admin',  AdminRoutes)
+app.use('/api/region', RegionRoutes);
+app.use('/api/admin', AdminRoutes);
 
-app.listen(PORT, () => {
-    console.log(`APP running at port ${PORT}`);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
 });
+
+// Server
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
+
+// Graceful shutdown
 process.on('SIGINT', async () => {
+    console.log('Closing server...');
+    await mongoose.disconnect();
+    process.exit(0);
+});
+process.on('SIGTERM', async () => {
     console.log('Closing server...');
     await mongoose.disconnect();
     process.exit(0);
