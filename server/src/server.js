436a3ada9bcf93c5071050bbba8f4524a2075cad
Dina <d.mechraoui8@gmail.com>
2024-12-29 06:36:35 +0100

    setting up server side

--- server/src/server.js
+++ server/src/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use(helmet());
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', 'https://kl-collection-2bfskr00f-dina-mechraouis-projects.vercel.app/'];
+const allowedOrigins = ['https://kl-collection-2bfskr00f-dina-mechraouis-projects.vercel.app/'];
 
 const options = {
   origin: function(origin, callback) {
