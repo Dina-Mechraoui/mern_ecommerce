80bfff9b573818ea6d0954185350031dcc08a9fa
Dina <d.mechraoui8@gmail.com>
2024-12-30 20:22:36 +0100

    server modified

--- client/src/Pages/ProductDetailsPage.jsx
+++ client/src/Pages/ProductDetailsPage.jsx
@@ -97,7 +97,7 @@ const ProductDetailsPage = () => {
     try {
       const response = await fetch(`${apiUrl}/api/cart/addToCart`, {
         method: "POST",
-        headers: { "Content-Type": "application/json", 'Origin': 'https://kl-collection-2bfskr00f-dina-mechraouis-projects.vercel.app', },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(cartItem),
         credentials: "include",
       });
